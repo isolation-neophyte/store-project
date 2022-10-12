0aa5bed76a24dff0666081a44a753f77fd09ed90
isneo <ebanyisneo@gmail.com>
2022-10-12 18:39:07 +0700

    updated and registered models Product and Product category, created categories and products

--- products/models.py
+++ products/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 
 class ProductCategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Product categories"
+
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Категория {self.name} - {self.description}"
 
 
 class Product(models.Model):
@@ -15,3 +22,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} | {self.category.name}"
